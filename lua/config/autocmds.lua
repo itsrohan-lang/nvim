3040682f6cf2335f7e9e191979aa421e84231c46
RDX463 <balsarafrohan627@gmail.com>
2026-06-26 16:25:02 +0530

    New nvim configuration

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,30 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", "*.mjs", "*.cjs", "*.md", "*.yaml" },
+  callback = function()
+    vim.lsp.buf.code_action({
+      context = { only = { "source.fixAll.oxc" }, diagnostics = {} },
+      apply = true,
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.html",
+  callback = function(args)
+    local first_lines = vim.api.nvim_buf_get_lines(args.buf, 0, 20, false)
+    for _, line in ipairs(first_lines) do
+      if line:match("{%%") or line:match("{{") or line:match("^%-%-%-") then
+        vim.bo[args.buf].filetype = "liquid"
+        return
+      end
+    end
+  end,
+})
---
+++ lua/config/options.lua
@@ -0,0 +1,30 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.completeopt = { "noinsert", "menuone", "noselect" }
+vim.opt.cursorline = true
+vim.opt.hidden = true
+vim.opt.autoindent = true
+vim.opt.inccommand = "split"
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.title = true
+vim.opt.wildmenu = true
+vim.opt.spell = true
+vim.opt.ttyfast = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.relativenumber = false
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.showmode = false
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.syntax = "on"
+vim.opt.wildoptions:append("pum")
